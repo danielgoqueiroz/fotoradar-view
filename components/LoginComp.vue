fcee27c5ca920d50e3091444b73bbb01da84e5e8
danielgoqueiroz <contato@danielqueiroz.com>
2022-05-29 12:10:13 -0300

    refactor: ajuste de páginas de login

--- components/LoginComp.vue
+++ components/LoginComp.vue
@@ -1,37 +1,51 @@
 <template>
   <b-container>
-    <b-form-input v-model="username" placeholder="Digite o usuário"/>
+    <b-form-input v-model="username" placeholder="Digite o usuário" />
     <b-form-input v-model="password" placeholder="Digite a senha" />
-    {{response}}
+    <b-button @click="login()">Enviar</b-button>
+    {{ token }}
   </b-container>
 </template>
 
 <script>
-import axios from '@nuxtjs/axios'
+import Axios from 'axios'
 
 export default {
   name: 'LoginComp',
-  data () {
+  data() {
     return {
-      username: '',
-      password: '',
-      response: {}
+      username: 'daniel',
+      password: 'senha123',
+      response: {},
+      token: '',
     }
-  },
-  mounted() {
-    this.login()
   },
   methods: {
     login() {
-      axios.post('http://localhost:8080/api/login',
-        new URLSearchParams({
-          username: 'daniel',
-          password: 'senha123',
-        }))
-        .then((res) => {
-          this.response = res;
-        });
-    }
-  }
+      const axios = Axios.create({
+        withCredentials: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          crossorigin: 'true',
+        },
+      })
+      axios
+        .post(
+          'http://localhost:8080/api/login',
+          new URLSearchParams({
+            username: this.username, // 'daniel'
+            password: this.password, // 'senha123'
+          })
+        )
+        .then((res, err) => {
+          if (err) {
+            console.err(err)
+          }
+          this.token = res.data.access_token
+          localStorage.setItem('token', res.data.access_token)
+          this.$router.push('welcome')
+        })
+    },
+  },
 }
 </script>
---
+++ pages/welcome.vue
@@ -0,0 +1,10 @@
+<template>
+  <b-container>
+    <h1>Helcome</h1>
+    Texto aqui
+  </b-container>
+</template>
+
+<script>
+export default { name: 'WelcomePage' }
+</script>
